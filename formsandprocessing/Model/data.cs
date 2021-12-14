f74f00e6250250e1d58860d6fcb5015f1b359d78
ash-0910 <A00244471@mycambrian.ca>
2021-12-13 20:40:26 -0500

    Add project files.

---
+++ formsandprocessing/Model/data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace formsandprocessing.Model
+{
+    public class data
+    {
+        public int Days { get; set; }
+
+        public DateTime Adddate { get; set; } 
+    }
+}
